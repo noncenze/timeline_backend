bfd52f4f5933214b5f71858308712f9e634026b0
evan-woodworth <evan.woodworth@gmail.com>
2021-06-28 10:05:48 -0700

    rest framework

---
+++ main_app/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name')
+
+class ExtendedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtendedUser
+        fields = ('id', 'user', 'profilepic')
+
+class DisplayTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DisplayType
+        fields = ('id', 'name')
+
+class TimelineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Timeline
+        fields = ('id', 'user', 'title', 'private')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'primary', 'timeline', 'displaytype')
+
+class EntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        fields = ('id', 'user', 'timeline', 'title', 'categories', 'datetime', 'summary', 'description', 'image')
--- timeline_backend/urls.py
+++ timeline_backend/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from main_app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView, 'user')
+router.register(r'extendedusers', views.ExtendedUserView, 'extendeduser')
+router.register(r'displaytypes', views.DisplayTypeView, 'displaytype')
+router.register(r'timelines', views.TimelineView, 'timeline')
+router.register(r'categories', views.CategoryView, 'category')
+router.register(r'entries', views.EntryView, 'entry')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
