371a9ae515928eee84721cf623c362e11ccec421
mchevez <mauricechevez@gmail.com>
2021-06-28 12:04:44 -0400

    First commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Timeline(models.Model):
+  name = models.CharField(max_length=100)
+
+class extendedUser(models.Model):
+  user = models.OneToOneField(User, on_delete=CASCADE)
+  email = models.CharField(max_length=100)
+  profile_pic = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.email
+  
+
