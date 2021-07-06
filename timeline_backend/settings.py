c0cef033973fe20d87de2facb22cc01f7dbe8777
evan-woodworth <evan.woodworth@gmail.com>
2021-07-05 19:01:53 -0700

    before deploy

--- timeline_backend/settings.py
+++ timeline_backend/settings.py
@@ -10,13 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from os import environ
+from pathlib import Path
 import dj_database_url
 import django_heroku
-from pathlib import Path
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
-config = dotenv_values(".env")
-print('( config )', config)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,13 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$)c@knnaaylrotp&md3=q!5ej%n#m-b-1qwksmquk16mvksh1'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'compendm.herokuapp.com'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
